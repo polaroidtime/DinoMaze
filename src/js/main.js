265e9b6d27cd7db687c3d6f293fcc7eec2d6ba03
polaroidtime <camilabetancourtb06@gmail.com>
2024-11-14 10:27:05 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ const size = 200,
     function startScene(){
         scene  = new THREE.Scene();
         scene.background = new THREE.Color(0xaed7f8);
-        camera = new THREE.PerspectiveCamera( 50  // Angulo de Vision (Abajo o Arriba)
+        camera = new THREE.PerspectiveCamera( 50,  // Angulo de Vision (Abajo o Arriba)
                                             window.innerWidth / window.innerHeight, // Relación Aspecto (16:9)
                                             0.1, // Mas Cerca (no renderiza)
                                             1000); // Mas lejos
@@ -64,3 +64,56 @@ function createModel(generalPath,pathMtl,pathObj,x,z,sc) {
     function addFigure(){
 
     }
+
+    function movePlayer(){
+        if(input.right == 1){
+          camera.rotation.y -= rotSpd;
+          MovingCube.rotation.y -= rotSpd;
+        }
+        if(input.left == 1){
+          camera.rotation.y += rotSpd; 
+          MovingCube.rotation.y += rotSpd;
+        }
+        
+         if(input.up == 1){
+          camera.position.z -= Math.cos(camera.rotation.y) * spd;
+          camera.position.x -= Math.sin(camera.rotation.y) * spd;
+    
+          MovingCube.position.z -= Math.cos(camera.rotation.y) * spd;
+          MovingCube.position.x -= Math.sin(camera.rotation.y) * spd;
+        }
+        if(input.down == 1){
+          camera.position.z += Math.cos(camera.rotation.y) * spd;
+          camera.position.x += Math.sin(camera.rotation.y) * spd;
+          
+          MovingCube.position.z += Math.cos(camera.rotation.y) * spd;
+          MovingCube.position.x += Math.sin(camera.rotation.y) * spd;
+        }
+      }
+      
+       window.addEventListener('keydown', function(e) {
+        switch (e.keyCode) {
+          case 68:
+            input.right = 1;
+            break;
+          case 65:
+            input.left = 1; 
+            break;
+          case 87:
+            input.up = 1;
+            sound1.play();
+            break;
+          case 83:
+            input.down = 1;
+            break;
+          case 27:
+            document.getElementById("blocker").style.display = 'block';
+            break;
+        }
+      });
+
+      function animate(){
+        requestAnimationFrame(animate);
+        movePlayer();
+      }
+    
